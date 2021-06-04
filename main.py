a1e6456857ecd5a21c766314a88bb436eab07495
chenBenTolila <chenbt266@gmail.com>
2021-06-04 14:10:13 +0300

    adding neville function

--- main.py
+++ main.py
@@ -11,4 +11,18 @@ def Lagrange(PointList, x):
     return sum
 
 pointList = [[1, 1], [2, 0], [4, 1.5]]
-print(Lagrange(pointList, 3))
+#print(Lagrange(pointList, 3))
+
+
+def neville(pointsList, m, n, X):
+    if m is n:
+        return pointsList[m][1]
+    else:
+        res = (X - pointsList[m][0]) * neville(pointsList, m+1, n, X) - (X - pointsList[n][0]) * neville(pointsList, m, n-1, X)
+        res /= (pointsList[n][0] - pointsList[m][0])
+        return res
+
+
+points = [[1, 1], [2, 4], [3, 9], [4, 16]]
+print(neville(points, 0, 3, 2.2))
+

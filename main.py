9ec17c1a8427b385bf13f49e4e015ff495e14443
chenBenTolila <chenbt266@gmail.com>
2021-06-04 14:14:31 +0300

    updating neville function

--- main.py
+++ main.py
@@ -18,11 +18,16 @@ def neville(pointsList, m, n, X):
     if m is n:
         return pointsList[m][1]
     else:
-        res = (X - pointsList[m][0]) * neville(pointsList, m+1, n, X) - (X - pointsList[n][0]) * neville(pointsList, m, n-1, X)
-        res /= (pointsList[n][0] - pointsList[m][0])
+        Xm = pointsList[m][0]
+        Xn = pointsList[n][0]
+        res = (X - Xm) * neville(pointsList, m+1, n, X) - (X - Xn) * neville(pointsList, m, n-1, X)
+        res /= (Xn - Xm)
         return res
 
 
 points = [[1, 1], [2, 4], [3, 9], [4, 16]]
 print(neville(points, 0, 3, 2.2))
 
+
+def polynomial(pointsList, X):
+    return
